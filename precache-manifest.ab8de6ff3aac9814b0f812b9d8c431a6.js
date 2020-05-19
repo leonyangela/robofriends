062eed63df4c13954f33f87296140949e7227c11
leonyangela <leonyangela@DESKTOP-1KNN1EJ>
2020-05-19 13:28:21 +0700

    Updates

---
+++ precache-manifest.ab8de6ff3aac9814b0f812b9d8c431a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddbed11c12c2a09c8638a3572354d0de",
+    "url": "/leonyangela/search-card.git/index.html"
+  },
+  {
+    "revision": "eff287cb371a28aa7b1c",
+    "url": "/leonyangela/search-card.git/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "bf0751794b04c8ce4ebc",
+    "url": "/leonyangela/search-card.git/static/css/main.90ad3baf.chunk.css"
+  },
+  {
+    "revision": "eff287cb371a28aa7b1c",
+    "url": "/leonyangela/search-card.git/static/js/2.8876ceb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/leonyangela/search-card.git/static/js/2.8876ceb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf0751794b04c8ce4ebc",
+    "url": "/leonyangela/search-card.git/static/js/main.5f414687.chunk.js"
+  },
+  {
+    "revision": "b19e6ce57b548fe60d0e",
+    "url": "/leonyangela/search-card.git/static/js/runtime-main.3e0cb514.js"
+  }
+]);
